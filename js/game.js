9cfcb17c5f49e9b21c430e9d2e15d22b1cf5e527
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-09 15:00:55 +0100

    game working in principle

--- js/game.js
+++ js/game.js
@@ -5,22 +5,86 @@
 // replicate the pattern.
 
 const buttonColors = ["red", "blue", "green", "yellow"];
-let gamePattern = [];
+let computerPattern = [];
+let userPattern = [];
+let gameStarted = false;
+let gameLevel = 0;
+
+// Press "s" to start the game. Listening on entire page for a keypress using jQuery.
+$(document).on("keydown", (event) => {
+    if (event.code === "KeyS" && event.key === "s") {
+        if (gameStarted === false) {
+            computerNextSequence();
+            gameStarted = true;
+        }
+    }
+});
+
+// Listen for player clicks on all buttons (ie: listen on all btn classes).
+// Note: cannot use arrow function as we need to access "this".
+$(".btn").click(function () {
+    // The IDs of the buttons are their colors.
+    const userChosenColor = $(this).attr("id");
+    // Store the chosen color in the player's pattern array.
+    userPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animateButton(userChosenColor);
+
+    checkAnswer(userPattern.length-1);
+});
+
+// Check if the computer and user patterns match.
+function checkAnswer(currLevel) {
+    if (userPattern[currLevel] === computerPattern[currLevel]) {
+        if (userPattern.length === computerPattern.length) {
+            setTimeout(function () { computerNextSequence(); }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over! Press \"s\"to play again");
+        setTimeout(function () { $("body").removeClass("game-over"); }, 200);
+        startOver();
+    }
+}
+
+// Computer adding another pattern to the sequence.
+function computerNextSequence() {
+    userPattern = [];
+    gameLevel++;
+    $("#level-title").text(`Level ${gameLevel}`);
+    // Computer is randomly selecting a color to add to the pattern.
+    const randomNumber = randomNumberGenerate(0, 3);
+    const randomChosenColor = buttonColors[randomNumber];
+    // Store the chosen color in the computer's pattern array.
+    computerPattern.push(randomChosenColor);
+    
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+// Animate a particular colored button using jQuery.
+function animateButton(col) {
+    $("#" + col).addClass("pressed");
+    setTimeout(function () {
+        $("#"+col).removeClass("pressed");
+    }, 100);
+}
+
+// Play sound associated with particular color.
+function playSound(col) {
+    const soundFile = `./sounds/${col}.mp3`;
+    (new Audio(soundFile)).play();
+}
 
 // Return a random number in the range min to max (inclusive of min and max).
 function randomNumberGenerate(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function nextSequence() {
-    const randomNumber = randomNumberGenerate(0,3);
-    const randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
-    // Animate the chosen color.
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    // Play sound associated with the chosen color.
-    const soundFile = `./sounds/${randomChosenColor}.mp3`;
-    (new Audio(soundFile)).play();
+function startOver() {
+    gameLevel = 0;
+    computerPattern = [];
+    gameStarted = false;
 }
-
-nextSequence();
