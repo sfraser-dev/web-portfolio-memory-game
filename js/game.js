ae316912c9b03b37291d5251c7ba4aa477dd0f5b
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-09 12:24:52 +0100

    favicon added. animating buttons and playing sound.

--- js/game.js
+++ js/game.js
@@ -1,13 +1,26 @@
-// A JS memory game using some jQuery. 
+// A JS memory game using some jQuery. Using four colored buttons, a pattern will begin
+// to be generated. It will start with one color, then two colours, then three colors,
+// etc. As the player continues to correctly replicate the pattern of colors, more and
+// more colors will be added (one at a time). The game ends when the player cannot 
+// replicate the pattern.
 
-// Test jQuery integration.
-$("h1").css("color", "pink");
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
 // Return a random number in the range min to max (inclusive of min and max).
-function randomNumber(min, max) {
+function randomNumberGenerate(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function nextSequence() {
-    const randNo = randomNumber(0,3);
+    const randomNumber = randomNumberGenerate(0,3);
+    const randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    // Animate the chosen color.
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    // Play sound associated with the chosen color.
+    const soundFile = `./sounds/${randomChosenColor}.mp3`;
+    (new Audio(soundFile)).play();
 }
+
+nextSequence();
